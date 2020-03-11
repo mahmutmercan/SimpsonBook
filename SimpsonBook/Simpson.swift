f4638cc6c79a6250e1f3abacebddc010a22d3a2d
mahmutMercan <mahmutmercan12@gmail.com>
2020-03-11 18:40:45 +0300

    Last
    ..

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,23 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by İlker isa Mercan on 8.12.2019.
+//  Copyright © 2019 Mahmut Mercan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName : String , simpsonJob : String , simpsonImage : UIImage) {
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+}
